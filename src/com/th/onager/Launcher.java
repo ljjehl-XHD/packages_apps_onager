d31cb721594b3a0e96171b41b99e1355cdd0707a
Jorge Ruesga <jorge@ruesga.com>
2013-04-07 03:45:13 +0200

    Trebuchet: Fix NPE
    This change fixes a NPE for the overflow menu. Steps:
    1.- A device with no physical menu
    2.- Install another launcher (xe Launcher2) (overflow menu is only displayed when Trebuchet runs
    outside CM or when multiple launchers exists)
    3.- Access AllAppDrawer and tap the overflow menu
    4.- Tap again the overflow menu or rotate the screen and tap the overflow menu.
    5.- NPE
    
    The overflow menu is not recreated in onCreateOptionsMenu when isWorkspaceLocked(). The change
    checks if the user is on the AllAppDrawer and forces the recreation of the menu.
    
    Change-Id: Id68dd1f7b0c09a2f62b5f6d4fbef6f217bf98e82
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/th/onager/Launcher.java
+++ src/com/th/onager/Launcher.java
@@ -1759,7 +1759,7 @@ public final class Launcher extends Activity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (isWorkspaceLocked()) {
+        if (!isAllAppsVisible() && isWorkspaceLocked()) {
             return false;
         }
 
